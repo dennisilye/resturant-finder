8fdf0350c331a44153b54c1985b83215ca7d10f5
dennisilye <eriadian@gmail.com>
2022-03-29 16:29:37 +0300

    added api call

--- App.js
+++ App.js
@@ -1,61 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, FlatList} from "react-native";
-import { CategoryItem } from "./src/components/CategoryItem";
-import { Header } from "./src/components/Header";
-import { Search } from "./src/components/Search";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import RestaurantScreen from "./src/screens/RestaurantScreen";
 
-export default function App() {
-  const commonCategories = [
-    {
-      name: "Burger",
-      imageUrl: require("./src/assets/images/burger.png"),
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Restaurant: RestaurantScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "BusinessSearch",
     },
-    {
-      name: "Dessert",
-      imageUrl: require("./src/assets/images/cake.png"),
-    },
-    {
-      name: "Pasta",
-      imageUrl: require("./src/assets/images/pasta.png"),
-    },
-    {
-      name: "Pizza",
-      imageUrl: require("./src/assets/images/pizza.png"),
-    },
-    {
-      name: "Smoothies",
-      imageUrl: require("./src/assets/images/smoothies.png"),
-    },
-    {
-      name: "Steak",
-      imageUrl: require("./src/assets/images/steak.png"),
-    },
-  ];
+  }
+);
 
-  return (
-    <View>
-      <Header />
-      <Search />
-      <FlatList
-      data={commonCategories}
-      renderItem={({item})=> {
-        return <CategoryItem name={item.name} imageUrl={item.imageUrl} />
-      }}
-      horizontal
-      showsVerticalScrollIndicator={false}
-      keyExtractor={(category) => category.name}
-      />
-
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+export default createAppContainer(navigator);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import { View,Text,StyleSheet } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export const Header = () => {
+export default function Header({ upperText, lowerText }) {
   return (
-    <View style={styles.container}>
-        <Text style={styles.lightHeader}>Grab your</Text>
-        <Text style={styles.boldHeader}>Delicous meal!</Text>
+    <View syle={styles.headerContainer}>
+      <Text style={styles.header1}>{upperText}</Text>
+      <Text style={styles.header2}>{lowerText}</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 60,
-        marginHorizontal: 25
-    },
-    lightHeader : {
-        fontSize:35,
-       
-    },
-    boldHeader : {
-        fontSize:40,
-        fontWeight: "bold",
-        
-    },
-})
+  header1: {
+    fontSize: 35,
+    marginTop: 60,
+    marginHorizontal: 25,
+  },
+  header2: {
+    fontSize: 40,
+    marginHorizontal: 25,
+    fontWeight: "bold",
+  },
+});
